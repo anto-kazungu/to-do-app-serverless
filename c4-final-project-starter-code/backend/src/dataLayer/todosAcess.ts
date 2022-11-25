1f69928e8e3b5522498e76b4b447113cd9dd5b98
anto-kazungu <kazoga.antony@gmail.com>
2022-11-25 17:34:47 +0300

    implimented create-todo function

---
+++ c4-final-project-starter-code/backend/src/dataLayer/todosAcess.ts
@@ -0,0 +1,56 @@
+import * as AWS from 'aws-sdk'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import { createLogger } from '../utils/logger'
+import { TodoItem } from '../models/TodoItem'
+//import { TodoUpdate } from '../models/TodoUpdate';
+//import AWSXRay from "aws-xray-sdk-core";
+const AWSXRay = require('aws-xray-sdk')
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const logger = createLogger('TodosAccess')
+
+// TODO: Implement the dataLayer logic
+export class TodosAccess {
+    constructor(
+        private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
+        private readonly todosTable = process.env.TODOS_TABLE,
+        private readonly todosIndex = process.env.INDEX_NAME
+    ){}
+
+    async getAllTodos(userId: string): Promise<TodoItem[]> {
+        logger.info('get all todos function called')
+
+        const result = await this.docClient
+        .query({
+            TableName: this.todosTable,
+            IndexName: this.todosIndex,
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {
+                ':userId': userId
+            }
+        })
+        .promise()
+
+        const items = result.Items
+        return items as TodoItem[]
+    }
+
+    //crete todo function
+
+    async createTodoItem(todoItem: TodoItem): Promise<TodoItem> {
+        logger.info('Create todo item function called')
+
+        const result = await this.docClient
+        .put({
+            TableName: this.todosTable,
+            Item: todoItem
+        })
+        .promise()
+        logger.info('Todo item created', result)
+
+        return todoItem as TodoItem
+    }
+
+    //update to do function
+}
